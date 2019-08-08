50bf646973273c79deb935639b955cd531698e06
Mete Kamil <mete@metekamil.com>
2019-08-07 21:22:43 -0500

    Closes issue #3
    Pulled in changes. Updated help text for access code and IP update requirements.

--- reboot.js
+++ reboot.js
@@ -1,6 +1,5 @@
-
 /* Fill in the access code and your router IP below */
-var routerAccessCode = "";
+var routerAccessCode = "put_your_router_access_code_here";
 var routerIP = "192.168.1.254";
 /****************************************************/
 
@@ -11,17 +10,17 @@ function RebootRouter() {
 
     var form = {
         "ADM_PASSWORD" : routerAccessCode,
-        "NEXTPAGE" : 'C_5_7' 
+        "NEXTPAGE" : 'A_0_0'
     };
 
     var formData = querystring.stringify(form);
     var contentLength = formData.length;
-    var cookieJar = request.jar();  
+    var cookieJar = request.jar();
     request({
 
         headers: {
-        'Content-Length': contentLength,
-        'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
         uri: 'http://' + routerIP + '/xslt?PAGE=login_post',
         jar: cookieJar,
@@ -30,53 +29,51 @@ function RebootRouter() {
     }, function (err, res, body) {
 
         if (body===undefined) {
-            console.log('Did not detect your gateway, please make sure you have specified your router IP using routerIP on line 4 inside reboot.js')
+            console.log('Did not detect your gateway, please make sure you have specified your router access code + router IP using routerIP on lines 3 & 4 inside reboot.js')
             return;
         }
 
         var nonce=body.substr(body.indexOf("NONCE")).toLowerCase();
         nonce=nonce.substr(nonce.indexOf("value=")+7);
-        nonce=nonce.substr(0,nonce.indexOf("\"")); 
+        nonce=nonce.substr(0,nonce.indexOf("\""));
 
         doRestart(cookieJar,nonce);
-        
+
     });
 }
-  
+
 function doRestart(cookieJar, nonce){
- 
+
     var form = {
-        "RESTART" : 'Reset', 
-        "THISPAGE" : 'C_5_7',
-        "NONCE": nonce, 
-        "BB_TYPE": "DHCP",
-        "WIFI0_EXIST": "TRUE"
+        "RESET_BB" : 'Restart',
+        "THISPAGE" : 'A_0_0',
+        "NEXTPAGE" : 'A_0_0_POST',
+        "NONCE": nonce,
+        "CMSKICK": ""
     };
     var formData = querystring.stringify(form);
     var contentLength = formData.length;
 
     request({
         headers: {
-        'Content-Length': contentLength,
-        'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
-        uri: 'http://' + routerIP + '/xslt?PAGE=C_5_7_POST&NEXTPAGE=C_5_7_POST',
+        uri: 'http://' + routerIP + '/xslt?PAGE=A_0_0_POST&NEXTPAGE=A_0_0_POST',
         jar: cookieJar,
         body: formData,
         method: 'POST'
-    }, function (err, res, body) { 
+    }, function (err, res, body) {
 
         if (err) {
-            console.log('Failed Reboot.');  
-            process.exit(0);
+            console.log('Failed Reboot.');
             return;
         }
 
-        console.log('Router Rebooting.');  
-        process.exit(0);
-        return;
+        console.log('Router Rebooting.');
 
     });
+
 }
- 
+
 RebootRouter();
